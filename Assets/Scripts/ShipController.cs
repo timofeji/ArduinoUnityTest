870c7d615cc26fa2f34ec47adeadc9923baad77c
Timofej Jermolaev <teemozy9@gmail.com>
2016-09-11 23:00:22 -0400

    init

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class ShipController : MonoBehaviour {
+
+	public GameObject shootProjectile;
+
+	public float shootTimer = 0.0f;
+	public float shootDelay = 1.0f;
+
+
+	SerialPort stream = new SerialPort("/dev/cu.wchusbserial1420", 9600);
+
+	void Start(){
+		stream.Open ();
+		stream.ReadTimeout = 1;
+	}
+
+	void Update () {
+
+		shootTimer += Time.deltaTime;
+		if (shootTimer >= shootDelay) {
+			Shoot();
+			shootTimer = 0.0f;
+		}
+		string value = stream.ReadLine(); 
+
+	}
+
+	void moveShip(string s){
+		Debug.Log (s);
+	}
+
+	void Shoot(){
+
+		GameObject projectile = (GameObject)Instantiate(shootProjectile, transform.position, transform.rotation);
+	}
+
+
+
+
+}
---
+++ Assets/projectileManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class projectileManager : MonoBehaviour {
+
+	public float projectileSpeed = 2.0f;
+
+	BoxCollider2D boxCollider;
+
+
+	// Update is called once per frame
+	void Update () {
+		transform.position += Vector3.right * projectileSpeed * 2 * Time.deltaTime;
+	}
+}
